3c23c3eaec1f0ed8c7f42c3b22fb50ebac497479
Ropyxa <godzilaluka@gmail.com>
2026-02-06 22:47:16 +0200

    update

--- kubejs/server_scripts/add.js
+++ kubejs/server_scripts/add.js
@@ -67,7 +67,7 @@ ServerEvents.recipes(e => {
 		'ABA',
 		'AAA'], {
 		A: 'casting:black_brick',
-		B: 'ae2:quartz_glass'
+		B: 'casting:black_brick_glass'
 	}).id('casting:tank');
 
 	e.shaped('industrialupgrade:itemcell/itemcell', [
@@ -75,11 +75,47 @@ ServerEvents.recipes(e => {
 		'ABA',
 		' A '], {
 		A: 'minecraft:copper_ingot',
-		B: 'ae2:quartz_glass'
+		B: 'casting:black_brick_glass'
 	}).id('industrialupgrade:industrialupgrade_1667');
 	
-	e.smelting('ae2:quartz_glass', 'kubejs:charcoal_sand_compound').id('ae2:decorative/quartz_glass');
+	e.smelting('casting:black_brick_glass', 'kubejs:charcoal_sand_compound').id('casting:black_brick_glass');
 	e.smelting('casting:black_brick', 'kubejs:cast_compound').id('casting:smelting/black_brick');
 	
 	e.recipes.casting.solidifier('kubejs:primitive_detector_template', '360x casting:molten_iron', 'kubejs:primitive_detector_furnace');
+});
+
+ServerEvents.tags('block', e => {
+	function RemoveAddToolTire(oldTool, newTool, block) {
+		e.remove(`minecraft:needs_${oldTool}_tool`, block)
+		e.add(`minecraft:needs_${newTool}_tool`, block)
+	};
+
+	const stoneToolBlocks = e.get('minecraft:needs_stone_tool').getObjectIds();
+
+	stoneToolBlocks.forEach(ore => {
+		if (
+			Ingredient.of(/^industrialupgrade:baseore1\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:baseore2\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockpreciousore\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:apatite\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:classicore\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:thorium_ore\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockspace\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockspace2\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockspace3\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockspace4\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:basaltheavyore\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:basaltheavyore1\/.+$/).test(ore) ||
+			Ingredient.of(/^industrialupgrade:blockradiationore\/.+$/).test(ore))
+		{
+			RemoveAddToolTire('stone', 'iron', ore);
+		}
+	});
+
+	e.add('minecraft:needs_iron_tool', 'minecraft:coal_ore');
+	e.add('minecraft:needs_iron_tool', 'minecraft:deepslate_coal_ore');
+	e.add('minecraft:needs_iron_tool', 'minecraft:nether_gold_ore');
+	e.add('minecraft:needs_iron_tool', 'minecraft:nether_quartz_ore');
+	RemoveAddToolTire('stone', 'iron', 'minecraft:lapis_ore');
+	RemoveAddToolTire('stone', 'iron', 'minecraft:deepslate_lapis_ore');
 });
